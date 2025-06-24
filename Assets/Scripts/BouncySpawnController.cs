95f03c243fa6e78302377019779b4612697fce32
Furai92 <joanjulian92@gmail.com>
2025-06-24 18:19:01 +0200

    Models adjusted

--- Assets/Scripts/BouncySpawnController.cs
+++ Assets/Scripts/BouncySpawnController.cs
@@ -11,7 +11,10 @@ public class BouncySpawnController : MonoBehaviour
     private int bouncesDone;
     private float bounceT;
     public bool BounceAnimationFinished { get; private set; }
+    private bool rotationLeft;
+    private float currentRotation;
 
+    private const float ROTATION_SPEED = 200f;
     private const float SPAWN_MOVEMENT_SPEED = 10f;
     private const float HEIGHT_MAX = 2.5f;
     private const float HEIGHT_MIN = 0.8f;
@@ -25,6 +28,13 @@ public class BouncySpawnController : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, Random.Range(0, 361), 0);
         bounceT = 0;
         BounceAnimationFinished = false;
+        rotationLeft = Random.Range(0, 2) == 0;
+        currentRotation = Random.Range(0, 361);
+    }
+    private void Update()
+    {
+        currentRotation += Time.deltaTime * (rotationLeft ? -ROTATION_SPEED : ROTATION_SPEED);
+        visualParent.localRotation = Quaternion.Euler(0, currentRotation, 0);
     }
 
     private void FixedUpdate()
